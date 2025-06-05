7fd4ae21d313252d516a67fe10e7f0044dafcbee
Matt Hiddle <matt.hiddle@gmail.com>
2025-06-05 17:28:23 +0800

    Add support for loading a League from io.ReadSeeker

---
+++ file_system_store.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"io"
+)
+
+type FileSystemPlayerStore struct {
+	database io.ReadSeeker
+}
+
+func (f *FileSystemPlayerStore) GetLeague() []Player {
+	f.database.Seek(0, io.SeekStart)
+	league, _ := NewLeague(f.database)
+	return league
+}
