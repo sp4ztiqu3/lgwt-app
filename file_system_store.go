c1e3748acdb26235a30b4a6d4267db994cf5476a
Matt Hiddle <matt.hiddle@gmail.com>
2025-06-10 18:07:53 +0800

    Sort output of /league

--- file_system_store.go
+++ file_system_store.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sort"
 )
 
 type FileSystemPlayerStore struct {
@@ -13,6 +14,9 @@ type FileSystemPlayerStore struct {
 }
 
 func (f *FileSystemPlayerStore) GetLeague() League {
+	sort.Slice(f.league, func(i, j int) bool {
+		return f.league[i].Wins > f.league[j].Wins
+	})
 	return f.league
 }
 
